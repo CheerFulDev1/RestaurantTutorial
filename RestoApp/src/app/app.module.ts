2915e5dbb9da63d1777059cc8179fa705c989b82
CheerFulDev <35979863+CheerFulDev1@users.noreply.github.com>
2021-02-13 13:21:40 +0100

    adding links

--- RestoApp/src/app/app.module.ts
+++ RestoApp/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRestoComponent,
+    UpdateRestoComponent,
+    ListRestoComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ RestoApp/src/app/update-resto/update-resto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-resto',
+  templateUrl: './update-resto.component.html',
+  styleUrls: ['./update-resto.component.css']
+})
+export class UpdateRestoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
