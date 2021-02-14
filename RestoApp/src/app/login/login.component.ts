b513e75d70a403a6b7a57ee340c04da9879222c7
CheerFulDev <35979863+CheerFulDev1@users.noreply.github.com>
2021-02-14 14:35:13 +0100

    basic login page

--- RestoApp/src/app/login/login.component.ts
+++ RestoApp/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  login = new FormGroup({
+    name: new FormControl(''),
+    address: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  checkUser(){
+    
   }
 
 }
